113362147b4d5b01c19d1faea8761cc734f04713
mishrikalariya088-creator <mishrikalariya088@gmail.com>
2026-01-19 20:38:56 +0530

    Add files via upload

---
+++ Practical Task1.py
@@ -0,0 +1,57 @@
+'''p = int(input("Enter principal: "))
+r = int(input("Enter rate: "))
+t = int(input("Enter time: "))
+
+si = (p * r * t) / 100
+print("Simple Interest =", si)
+======================================='''
+
+'''a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Maximum number =", a)
+else:
+    print("Maximum number =", b)
+======================================'''
+'''
+for i in range(1, 6):
+    print(i)
+====================='''
+'''
+s = input("Enter a string: ")
+print("Length of string =", len(s))
+
+========'''
+'''print("Welcome to Python Programming")
+============================================'''
+'''
+s = input("Enter a string: ")
+print("First character =", s[0])
+=================================='''
+
+'''s = input("Enter a string: ")
+print("Last character =", s[-1])
+======================================'''
+'''
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("Positive number")
+else:
+    print("Negative number")
+======================'''
+'''
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+sum = a + b + c
+print("Sum =", sum)
+=============================='''
+'''
+name = input("Enter your name: ")
+print("Hello", name)
+========================'''
+
+
