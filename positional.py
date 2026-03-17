42f306fc5229f1d85d92de2223a590c01d807db1
mishrikalariya088-creator <mishrikalariya088@gmail.com>
2026-03-17 09:11:01 +0530

    Add files via upload

---
+++ positional.py
@@ -0,0 +1,14 @@
+#1 basic positional
+def add(a,b):
+    print("a=",a)
+    print("b=",b)
+    return a+b
+result=add(2,50)
+print("sum",result)
+
+#2 student information
+def student_info(name,roll,marks):
+    print("name:",name)
+    print("roll no:",roll)
+    print("marks:",marks)
+student_info("ravi",101,95)
