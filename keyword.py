8b576bf2ecdd85f2eff51448a72ded4eef84baec
mishrikalariya088-creator <mishrikalariya088@gmail.com>
2026-03-17 09:21:54 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,13 @@
+#1 basic keyword
+def student_info(name,age,city):
+    print("name:",name)
+    print("age:",age)
+    print("city:",city)
+atudent_info(age=19,city="rajkot",name="ravi")
+
+#2 mixing positional and keyword
+def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(1,c=3,b=2)
