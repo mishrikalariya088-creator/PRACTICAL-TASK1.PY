4ce0bce5f73ccdf1877d0a58ac1f15671e8fb2a2
mishrikalariya088-creator <mishrikalariya088@gmail.com>
2026-03-31 07:43:22 +0530

    Add files via upload

---
+++ defolt.py
@@ -0,0 +1,14 @@
+#basic default argument
+
+def greet(name, msg="good morning"):
+    print("hello",name +",",msg)
+    greet ("asha")
+    greet ("ravi","good morning")
+
+#2
+ 
+def_power (num,exp=2)
+retorn num**exp
+print(power(3))
+print(poeer(3,3))
+print(power(2,4))
